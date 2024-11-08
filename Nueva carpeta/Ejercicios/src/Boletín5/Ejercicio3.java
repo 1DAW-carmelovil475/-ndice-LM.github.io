bd764ff01b7304ae4ef1fc3afeb81bdcecf70de6
1DAW-carmelovil475 <cvilpiz939@g.educaand.es>
2024-11-08 08:54:25 +0100

    Delete Nueva carpeta directory

--- Nueva carpeta/Ejercicios/src/Boletín5/Ejercicio3.java
+++
@@ -1,9 +0,0 @@
-package Boletín5;
-
-public class Ejercicio3 {
-    public static void main(String[] args){
-        for (int i= 0; i<20; i++){
-            System.out.println(2*i);
-        }
-    }
-}
